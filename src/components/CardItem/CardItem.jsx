60fe5754d1266c358f2d5dcc522b905d6335e036
unknown <liranalon84@gmail.com>
2023-02-06 12:20:20 +0200

    add grid to cards wrapper

--- src/components/CardItem/CardItem.jsx
+++ src/components/CardItem/CardItem.jsx
@@ -6,7 +6,7 @@ export default function CardItem({item, index, title}) {
         <>
         <div className="card" key={item.id}>
             <div className="cover">
-                <img src={item.images[0]?.url} width="150" height="150" alt={item.name} />
+                <img src={item.images[0]?.url} width="100%" height="100%" alt={item.name} />
                 <div className="play-icon">
                     <svg role="img" height="24" width="24" aria-hidden="true" viewBox="0 0 24 24" data-encore-id="icon"><path d="M7.05 3.606l13.49 7.788a.7.7 0 010 1.212L7.05 20.394A.7.7 0 016 19.788V4.212a.7.7 0 011.05-.606z"></path></svg>
                 </div>
