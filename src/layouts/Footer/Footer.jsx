ab46c75290a63f598a4552dd8977debe3fcea32d
unknown <liranalon84@gmail.com>
2023-02-17 23:53:18 +0200

    initial fortawesome

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -1,9 +1,13 @@
 import "./Footer.scss"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 export default function Footer() {
     return (
         <footer>
-            
+            <div className="container">
+                <FontAwesomeIcon icon={faEnvelope} /> 
+            </div>
         </footer>
     )
 }
